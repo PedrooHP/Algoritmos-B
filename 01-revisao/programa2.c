13e62945ba07922074f6352936cd79ec7b9fe138
PedroHL <161592623+PedrooHP@users.noreply.github.com>
2025-07-28 23:29:50 +0000

    programa

--- 01-revisao/programa2.c
+++ 01-revisao/programa2.c
@@ -6,7 +6,7 @@ int main (){
     srand(time(NULL)); //semeador
 
     int NumeroSorteado = rand() % 100; //sorteando numero entra 0 e 100
-    printf ("%d\n" NumeroSorteado);
+    printf ("%d\n", NumeroSorteado);
 
     return 1;
 }
