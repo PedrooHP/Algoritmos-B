a74a5d5f0b8349e87bb6236cc65e8ce9d2f267c7
PedroHL <161592623+PedrooHP@users.noreply.github.com>
2025-07-28 23:53:29 +0000

    programa

---
+++ Exercícios/Exe1.c
@@ -0,0 +1,25 @@
+//sortear TAM n°s (até TA n°s) no vetor, porém não inserir n°s já presentes no vetor. Ao final, exibir o vetor
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define TAM 5
+
+int main (){
+    int vetor[TAM];
+
+    srand(time(NULL));
+
+    for (int i = 0; i < TAM; i++){
+        vetor[i] = rand() % 100;
+    }
+
+    for (int i = 0; i < TAM; i++){
+        printf("%d\t", vetor[i]);
+    }
+
+    printf("\n");
+    
+    return 1;
+}
