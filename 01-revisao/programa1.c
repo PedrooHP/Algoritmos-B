ea87ffb5f736dd4b62b378ee13c6d33031bf7e27
PedroHL <161592623+PedrooHP@users.noreply.github.com>
2025-07-28 22:55:43 +0000

    programa1

--- 01-revisao/programa1.c
+++ 01-revisao/programa1.c
@@ -3,7 +3,12 @@
 #include <time.h>
 
 int main (){
+    system("clear");//Limpar terminal no linux
+    
     printf("Boa noite\n");
     return 1;
 
 }
+
+//compilando arquivo c ---> gcc programa1.c -o roda
+//executando esse arquivo ---> ./roda
