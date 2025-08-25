041579701a40b6cc2b2c71950bf8e02e8ad98932
PedroHL <161592623+PedrooHP@users.noreply.github.com>
2025-08-25 20:24:07 -0300

    Modularização

---
+++ Modularização/metodoSemRetorno1.cpp
@@ -0,0 +1,23 @@
+#include <cstdlib>
+#include <time.h>
+#include <iostream>
+
+#define TAM 10000
+
+using namespace string horaSaida;
+
+#include "metodos.h"
+
+int main(){
+    int vetor [TAM];
+    int quantidade = 10;
+
+    //metodo que popule o vetor com elementos (aleatórios) desde que o menor do que TAM
+    popular (vetor, quantidade, TAM);
+
+    //metodo que exiba o vetor
+    exibir("Exibindo vetor", vetor, quantidade, TAM);
+
+    //metodo que receba um vetor, quantidade e seu tamanho; e exiba o menor elemento
+    return 1;
+}
