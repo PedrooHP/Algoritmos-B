ceda369fdd006eaddd9fc47aa1d2dd330b952200
PedroHL <161592623+PedrooHP@users.noreply.github.com>
2025-08-25 20:40:33 -0300

    Metodos.h

--- Modularização/metodos.h
+++ Modularização/metodos.h
@@ -2,15 +2,42 @@
 #include <time.h>
 #include <string.h>
 
-void popular (int vetor[], int quantidade, int tamanho){
-    if (quantidade > tamanho){
-        cout << "Essa quantidade não eh possivel...";
+void popular(int vetor[], int quantidade, int tamanho) {
+    if (quantidade > tamanho) {
+        cout << "Essa quantidade nao eh possivel...";
         return; //forcando a saida do metodo
     }
     srand(time(NULL));
-    for (int i = 0; i < quantidade; i++){
+    for (int i = 0; i < quantidade; i++) {
         vetor[i] = rand() % 100;
     }
 }
 
-void exibir (string frase, int vetor[], )
+void exibir(string frase, int vetor[], int quantidade, int tamanho) {
+    if (quantidade > tamanho) {
+        cout << "Essa quantidade nao eh possivel...";
+        return; //forcando a saida do metodo
+    }
+    cout << frase << endl;
+    for (int i = 0; i < quantidade; i++) {
+        cout << vetor[i] << endl;
+    }
+}
+
+void menorElemento(int vetor[], int quantidade, int tamanho) {
+    int menor = vetor[0];
+
+    if (quantidade > tamanho) {
+        cout << "Essa quantidade nao eh possivel...";
+        return; //forcando a saida do metodo
+    }
+
+    for (int i = 1; i < quantidade; i++) {
+        if (vetor[i] < menor) {
+            menor = vetor[i];
+        }
+    }
+
+    cout << "Menor elemento.... " << menor << endl;
+
+}
